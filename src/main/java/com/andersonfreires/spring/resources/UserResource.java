fdd229f242671aad1e2393ff5bb0da7a8c3ba2ba
Anderson freires <andersonfreires1979@gmail.com>
2026-06-19 12:33:48 -0300

    Insercao de usuario com post

--- src/main/java/com/andersonfreires/spring/resources/UserResource.java
+++ src/main/java/com/andersonfreires/spring/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.andersonfreires.spring.resources;
 
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andersonfreires.spring.domain.User;
 import com.andersonfreires.spring.dto.UserDTO;
@@ -31,11 +35,19 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
-	@GetMapping("/{id}")
+	@GetMapping ("/{id}")
 	public ResponseEntity<UserDTO>  findById(@PathVariable String id){
 		User obj=service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 		
+	}
+	
+		@PostMapping
+		public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/andersonfreires/spring/services/UserService.java
+++ src/main/java/com/andersonfreires/spring/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andersonfreires.spring.domain.User;
+import com.andersonfreires.spring.dto.UserDTO;
 import com.andersonfreires.spring.repositories.UserRepository;
 import com.andersonfreires.spring.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+				}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
 	}
 }
