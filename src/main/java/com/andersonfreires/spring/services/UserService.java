daa5ce16cc38ffd6f12957c758f877ff4c170e1a
Anderson freires <andersonfreires1979@gmail.com>
2026-06-19 15:30:22 -0300

    Atualização de usuario com put

--- src/main/java/com/andersonfreires/spring/services/UserService.java
+++ src/main/java/com/andersonfreires/spring/services/UserService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.andersonfreires.spring.domain.User;
 import com.andersonfreires.spring.dto.UserDTO;
 import com.andersonfreires.spring.repositories.UserRepository;
@@ -13,10 +12,12 @@ import com.andersonfreires.spring.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
+
 	
 	@Autowired
 	private  UserRepository repository;
-	
+
+
 	
 	public List<User> findAll(){
 	return	repository.findAll();
@@ -36,6 +37,18 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
 	}
