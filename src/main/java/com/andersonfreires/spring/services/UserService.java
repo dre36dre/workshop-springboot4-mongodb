e0ae00189d90a774f2135ab444999e6f625bebf8
Anderson freires <andersonfreires1979@gmail.com>
2026-06-19 12:14:32 -0300

    Obtendo o usuario por ID

--- src/main/java/com/andersonfreires/spring/services/UserService.java
+++ src/main/java/com/andersonfreires/spring/services/UserService.java
@@ -1,12 +1,14 @@
 package com.andersonfreires.spring.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andersonfreires.spring.domain.User;
 import com.andersonfreires.spring.repositories.UserRepository;
+import com.andersonfreires.spring.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,8 @@ public class UserService {
 	return	repository.findAll();
 	}
 
-	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
