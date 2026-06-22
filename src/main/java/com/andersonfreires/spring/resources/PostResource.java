91aceb1515b44b4228c025a849d48d626781f238
Anderson freires <andersonfreires1979@gmail.com>
2026-06-22 17:05:01 -0300

    Consulta simples com query methods

--- src/main/java/com/andersonfreires/spring/resources/PostResource.java
+++ src/main/java/com/andersonfreires/spring/resources/PostResource.java
@@ -2,14 +2,18 @@ package com.andersonfreires.spring.resources;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andersonfreires.spring.domain.Post;
+import com.andersonfreires.spring.resources.util.URL;
 import com.andersonfreires.spring.services.PostService;
 
 
@@ -26,6 +30,13 @@ public class PostResource {
 		Post obj=service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@GetMapping("/titlesearch")
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 		
--- src/main/java/com/andersonfreires/spring/services/PostService.java
+++ src/main/java/com/andersonfreires/spring/services/PostService.java
@@ -1,5 +1,6 @@
 package com.andersonfreires.spring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	
-	
+	public  List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 }
